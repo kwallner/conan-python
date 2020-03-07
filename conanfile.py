f796d4180fb50d979d5d72cd472d978a5dac6bac
Karl Wallner <kwallner@mail.de>
2020-03-07 14:42:49 +0100

    Add pip into package.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,6 @@
 import os
-#import sys
-#import shutil
-#import json
-#import distutils.dir_util 
-#import subprocess 
+import subprocess
+import shutil
 from conans import ConanFile, tools, errors
 
 def Python3ConanFile():
@@ -44,6 +41,8 @@ class ConanProject(ConanFile):
     description = "Python Programming Language Version 3"
     settings = "os", "arch"
     generators  = "txt"
+    no_copy_source = True
+    _pip_whl = "pip-20.0.2-py2.py3-none-any.whl"
 
     @property
     def python_interpreter(self):
@@ -59,13 +58,26 @@ class ConanProject(ConanFile):
                 raise errors.ConanInvalidConfiguration("Invalid architecture \"%s\" for os \"%s\"." % (self.settings.arch, self.settings.os))
         else:
             raise errors.ConanInvalidConfiguration("Unsupported os \"%s\"." % (self.settings.os))
+        tools.download("https://files.pythonhosted.org/packages/54/0c/d01aa759fdc501a58f431eb594a17495f15b88da142ce14b5845662c13f3/%s" % self._pip_whl, self._pip_whl, sha256="4ae14a42d8adba3205ebeb38aa68cfc0b6c346e1ae2e699a0b3bad4da19cef5c")
 
+    def _install_pip(self):
+        self.output.info("Installing pip from package \"%s\" to folder \"%s\"." % (self._pip_whl, self.build_folder))
+        shutil.copyfile(os.path.join(self.source_folder, self._pip_whl), self._pip_whl)
+        with tools.chdir(self.build_folder):
+            env_python = os.environ.copy()
+            env_python["PYTHONPATH"] = self.build_folder
+            subprocess.run([self.python_interpreter, "%s/pip" % self._pip_whl, "install", "--no-index", "--no-cache-dir", "--isolated", "--find-links=%s" % self.source_folder, self._pip_whl], cwd=self.build_folder, shell=True, check=True, env=env_python)
+        os.remove(self._pip_whl)
+       
     def build(self):
         if self.settings.os == "Windows":
-            tools.unzip(os.path.join(self.source_folder, "python.zip"), self.package_folder)
+            tools.unzip(os.path.join(self.source_folder, "python.zip"), self.build_folder)
+            os.remove("python37._pth")
         else:
             raise errors.ConanInvalidConfiguration("Unsupported os \"%s\"." % (self.settings.os))
-
+        self._install_pip()
+        shutil.copytree(self.build_folder, self.package_folder, ignore=shutil.ignore_patterns('__pycache__', 'Scripts'))
+        
     def package(self):
         pass
 
@@ -74,7 +86,9 @@ class ConanProject(ConanFile):
         self.cpp_info.libdirs = ['lib']
         self.env_info.PYTHONPATH = self.package_folder
         if self.settings.os == "Windows":
+            self.cpp_info.bindirs = ['.', 'Scripts']
             self.env_info.path.insert(0, self.package_folder)
+            self.env_info.path.insert(0, os.path.join(self.package_folder, "Scripts"))
         else:
             self.cpp_info.bindirs = ['bin']
             self.env_info.path.insert(0, os.path.join(self.package_folder, "bin"))
