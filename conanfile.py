20ec74c9a9c2f4f186ecf0f76e8aa70f38162d2f
Karl Wallner <kwallner@mail.de>
2020-03-08 09:34:21 +0100

    Correct install package layout. Remove base python package.

--- conanfile.py
+++ conanfile.py
@@ -2,44 +2,14 @@ import os
 import subprocess
 import shutil
 from conans import ConanFile, MSBuild, tools, errors, AutoToolsBuildEnvironment
-
-def Python3ConanFile():
-    class _Python3ConanFile(ConanFile):
-        #def build_requirements(self):
-        #    self.build_requires("python3/3.7.6@akw/testing")
-        #
-        #def requirements(self):
-        #    pass
-        @property
-        def python_command(self):
-            import os
-            for python3_bin_dir in self.deps_cpp_info["python-base"].bin_paths:
-                self.output.info("Searching for python-base in directory \"%s\"." % python3_bin_dir)
-                python_exe = os.path.join(python3_bin_dir, "python3%s" % (".exe" if tools.os_info.is_windows else ""))
-                if os.path.isfile(python_exe):
-                    self.output.info("Found python3 executable as \"%s\"." % python_exe)
-                    return python_exe
-                python_exe = os.path.join(python3_bin_dir, "python%s" % (".exe" if tools.os_info.is_windows else ""))
-                if os.path.isfile(python_exe):
-                    self.output.info("Found python3 executable as \"%s\"." % python_exe)
-                    return python_exe
-            raise errors.ConanInvalidConfiguration("python interpreter not found.")
-        
-        def python_run(self, *argv, **kwargs):
-            import subprocess 
-            command= [ self.python_command ]
-            for arg in argv: command.append(arg) 
-            self.output.info("Executing python3 (cwd=%s): %s" % (os.getcwd(), " ".join(command)))
-            subprocess.run(command, **kwargs)
-    return _Python3ConanFile
-
+    
 class ConanProject(ConanFile):
     name        = "python"
     version     = "3.7.6"
     url         = ""
     license     = "Python Software Foundation License Version 2"
     description = "Python Programming Language Version 3"
-    settings = "os", "arch", "compiler", "build_type"
+    settings = "os", "arch", "compiler"
     generators  = "txt"
     _pip_whl = "pip-20.0.2-py2.py3-none-any.whl"
 
@@ -80,14 +50,15 @@ class ConanProject(ConanFile):
                 self._pip_whl], cwd=self.package_folder, shell=True, check=True, env=env_python)
             shutil.rmtree("Scripts")
             os.remove(self._pip_whl)
-       
+        
     def build(self):
         with tools.chdir("Python-%s" % self.version):
             if self.settings.os == "Windows":
                 with tools.chdir("PCBuild"):
                     with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else tools.no_op():
                         msbuild = MSBuild(self)
-                        msbuild.build("pcbuild.sln")
+                        #msbuild.build("pcbuild.sln", build_type="Debug")
+                        msbuild.build("pcbuild.sln", build_type="Release")
             else:
                 os.chmod("configure", 
                     stat.S_IRUSR |
@@ -111,19 +82,22 @@ class ConanProject(ConanFile):
             pcbuild_folder = os.path.join(self.build_folder, "Python-%s" % self.version, "PCBuild", out_folder)
             pc_folder = os.path.join(self.build_folder, "Python-%s" % self.version, "PC")
             self.copy(pattern="*.dll", dst="bin", src=pcbuild_folder, keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src=pcbuild_folder, keep_path=False)
             self.copy(pattern="*.exe", dst="bin", src=pcbuild_folder, keep_path=False)
+            self.copy(pattern="*.lib", dst="libs", src=pcbuild_folder, keep_path=False)
+            self.copy(pattern="*.pyd", dst="DLLs", src=pcbuild_folder, keep_path=False)
             shutil.copytree(os.path.join(self.build_folder, "Python-%s" % self.version, "Include"), os.path.join(self.package_folder, "include"))
             self.copy(pattern="*.h", dst="include", src=pc_folder, keep_path=False)
+            shutil.copytree(os.path.join(self.build_folder, "Python-%s" % self.version, "Lib"), os.path.join(self.package_folder, "Lib"))
         self._install_pip()
 
     def package_id(self):
         del self.info.settings.compiler
-        del self.info.settings.build_type
         
     def package_info(self):
         self.cpp_info.includedirs = ['include']
-        self.cpp_info.libdirs = ['lib']
-        self.env_info.PYTHONPATH = os.path.join(self.package_folder, "lib") + os.pathsep + os.path.join(self.package_folder, "lib", "site-packages") 
+        self.cpp_info.libdirs = ['libs']
         self.cpp_info.bindirs = ['bin']
-        self.env_info.path.insert(0, os.path.join(self.package_folder, "bin"))
+        self.env_info.PATH.insert(0, os.path.join(self.package_folder, "bin"))
+        self.env_info.PYTHONPATH.insert(0, os.path.join(self.package_folder, "Lib"))
+        self.env_info.PYTHONPATH.insert(0, os.path.join(self.package_folder, "Lib", "site-packages"))
+        self.env_info.PYTHONHOME = self.package_folder
