4a3cddb99876fe1d1898c253025a40a976fe7e97
Karl Wallner <kwallner@mail.de>
2020-03-15 19:55:41 +0100

    Install packages wheel and setuptool. Fix installation of pip.

--- conanfile.py
+++ conanfile.py
@@ -5,13 +5,15 @@ from conans import ConanFile, VisualStudioBuildEnvironment, tools, errors, AutoT
     
 class ConanProject(ConanFile):
     name        = "python"
-    version     = "3.7.6"
+    version     = "3.7.7"
     url         = ""
     license     = "Python Software Foundation License Version 2"
     description = "Python Programming Language Version 3"
     settings = "os", "arch", "compiler"
     generators  = "txt"
     _pip_whl = "pip-20.0.2-py2.py3-none-any.whl"
+    _wheel_whl = "wheel-0.34.2-py2.py3-none-any.whl"
+    _setuptools_whl = "setuptools-46.0.0-py3-none-any.whl"
 
     @property
     def python_interpreter(self):
@@ -21,6 +23,8 @@ class ConanProject(ConanFile):
         if self.settings.os == "Windows":
             if self.settings.arch != "x86_64":
                 raise errors.ConanInvalidConfiguration("Platform is not supported.")
+            if self.settings.compiler != "Visual Studio" and self.settings.compiler.version != "15":
+                raise errors.ConanInvalidConfiguration("Compiler is not supported.")
 
     def system_requirements(self):
         if self.settings.os == "Linux":
@@ -29,17 +33,19 @@ class ConanProject(ConanFile):
             installer.install("libffi-dev") 
     
     def source(self):
-        tools.download("https://www.python.org/ftp/python/%s/Python-%s.tgz" % (self.version, self.version), "Python-%s.tgz" % self.version, md5="3ef90f064506dd85b4b4ab87a7a83d44")
+        tools.download("https://www.python.org/ftp/python/%s/Python-%s.tgz" % (self.version, self.version), "Python-%s.tgz" % self.version, sha256="8c8be91cd2648a1a0c251f04ea0bb4c2a5570feb9c45eaaa2241c785585b475a")
         tools.download("https://files.pythonhosted.org/packages/54/0c/d01aa759fdc501a58f431eb594a17495f15b88da142ce14b5845662c13f3/%s" % self._pip_whl, self._pip_whl, sha256="4ae14a42d8adba3205ebeb38aa68cfc0b6c346e1ae2e699a0b3bad4da19cef5c")
+        tools.download("https://files.pythonhosted.org/packages/8c/23/848298cccf8e40f5bbb59009b32848a4c38f4e7f3364297ab3c3e2e2cd14/%s" % self._wheel_whl, self._wheel_whl, sha256="df277cb51e61359aba502208d680f90c0493adec6f0e848af94948778aed386e")
+        tools.download("https://files.pythonhosted.org/packages/70/b8/b23170ddda9f07c3444d49accde49f2b92f97bb2f2ebc312618ef12e4bd6/%s" % self._setuptools_whl, self._setuptools_whl, sha256="693e0504490ed8420522bf6bc3aa4b0da6a9f1c80c68acfb4e959275fd04cd82")
         tools.unzip("Python-%s.tgz" % self.version)
         if tools.os_info.is_windows:  
             with tools.chdir(os.path.join("Python-%s" % self.version, "PCBuild")):
                 self.run("get_externals.bat")
         os.remove("Python-%s.tgz" % self.version)
 
-    def _install_pip(self):
+    def _install_pip(self, whl_file):
         with tools.chdir(self.package_folder):
-            shutil.copyfile(os.path.join(self.source_folder, self._pip_whl), self._pip_whl)
+            shutil.copyfile(os.path.join(self.build_folder, self._pip_whl), os.path.join(self.package_folder, self._pip_whl))
             self.output.info("Installing pip from package \"%s\" to folder \"%s\"." % (self._pip_whl, self.package_folder))
             env_python = os.environ.copy()
             env_python["PYTHONPATH"] = self.package_folder
@@ -52,10 +58,27 @@ class ConanProject(ConanFile):
                 "--find-links=%s" % self.build_folder, 
                 "--prefix=.", 
                 "--no-warn-script-location", 
-                self._pip_whl], cwd=self.package_folder, shell=True, check=True, env=env_python)
+                whl_file], cwd=self.package_folder, shell=True, check=True, env=env_python)
             shutil.rmtree("Scripts")
-            os.remove(self._pip_whl)
+            os.remove(os.path.join(self.package_folder, self._pip_whl))
         
+    def _install_whl(self, whl_file):
+        with tools.chdir(self.package_folder):
+            self.output.info("Installing pip from package \"%s\" to folder \"%s\"." % (whl_file, self.package_folder))
+            env_python = os.environ.copy()
+            env_python["PYTHONPATH"] = self.package_folder
+            subprocess.run([self.python_interpreter, 
+                "-m", "pip", 
+                "install", 
+                "--no-index", 
+                "--no-cache-dir", 
+                "--isolated", 
+                "--find-links=%s" % self.build_folder, 
+                "--prefix=.", 
+                "--no-warn-script-location", 
+                whl_file], cwd=self.package_folder, shell=True, check=True, env=env_python)
+            shutil.rmtree("Scripts")
+            
     def build(self):
         if self.settings.os == "Windows":
             env_build = VisualStudioBuildEnvironment(self)
@@ -64,10 +87,6 @@ class ConanProject(ConanFile):
                     vcvars = tools.vcvars_command(self.settings)
                     self.run("%s && cmd /C build.bat -p x64 -d" % vcvars)
                     self.run("%s && cmd /C build.bat -p x64" % vcvars)
-                #subprocess.run(["cmd", "/C", "build.bat", "-p", "x64", "-d"], check=True, cwd=os.path.join(self.build_folder, "Python-%s" % self.version, "PCBuild"))
-                #    #msbuild = MSBuild(self)
-                #    #msbuild.build("pcbuild.sln", build_type="Debug", arch ="x86_64")
-                #    #msbuild.build("pcbuild.sln", build_type="Release")
         else:
             with tools.chdir("Python-%s" % self.version):
                 os.chmod("configure", 
@@ -98,7 +117,9 @@ class ConanProject(ConanFile):
             shutil.copytree(os.path.join(self.build_folder, "Python-%s" % self.version, "Include"), os.path.join(self.package_folder, "include"))
             self.copy(pattern="*.h", dst="include", src=pc_folder, keep_path=False)
             shutil.copytree(os.path.join(self.build_folder, "Python-%s" % self.version, "Lib"), os.path.join(self.package_folder, "Lib"))
-        self._install_pip()
+        self._install_pip("pip")
+        self._install_whl("wheel")
+        self._install_whl("setuptools")
 
     def package_id(self):
         del self.info.settings.compiler
